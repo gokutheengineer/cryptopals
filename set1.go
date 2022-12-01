d496b5ea6021745e5d8799e7328ec851c2031185
gokutheengineer <engineerzubizaretta@gmail.com>
2022-12-01 21:20:50 +0300

    set1 problem 2

--- set1.go
+++ set1.go
@@ -3,7 +3,6 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 	"log"
 )
 
@@ -12,8 +11,22 @@ func HexToBase64(hs string) (string, error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", v)
+	//fmt.Printf("%s\n", v)
 
 	return base64.StdEncoding.EncodeToString(v), nil
 
+}
+
+func Xor(a, b []byte) []byte {
+	if len(a) != len(b) {
+		panic("Xor lengths are not equal")
+	}
+
+	res := make([]byte, len(a))
+
+	for ind, val := range a {
+		res[ind] = val ^ b[ind]
+	}
+
+	return res
 }
--- set1_test.go
+++ set1_test.go
@@ -1,13 +1,38 @@
 package cryptopals
 
-import "testing"
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
 
-func TestSet1(t *testing.T) {
+func TestSet1Problem1(t *testing.T) {
 	res, err := HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	if err != nil {
 		t.Error(err)
 	}
 	if res != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
-		t.Fatal("wrong result: ", res)
+		t.Error("wrong result: ", res)
 	}
+}
+
+func TestSet1Problem2(t *testing.T) {
+	first := HexDecode(t, "1c0111001f010100061a024b53535009181c")
+	second := HexDecode(t, "686974207468652062756c6c277320657965")
+
+	res := Xor(first, second)
+
+	if !bytes.Equal(res, HexDecode(t, "746865206b696420646f6e277420706c6179")) {
+		t.Errorf("Wrong result: %x", res)
+	}
+
+}
+
+func HexDecode(t *testing.T, hs string) []byte {
+	v, err := hex.DecodeString(hs)
+	if err != nil {
+		t.Fatal("Failed to decode hex: ", err)
+	}
+
+	return v
 }
